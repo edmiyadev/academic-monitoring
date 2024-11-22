7171eccb8b22cd0ccfb79c562ebd5fbfe7fcdbb2
Edward Minaya <edmiya.dev@gmail.com>
2024-11-22 18:35:49 -0400

    created Carrer in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Enums\EducationalInstitutionEnum;
+use App\Enums\EducationalLevelEnum;
+use App\Models\Career;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,5 +23,13 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
             'password' => 'password',
         ]);
+
+        Career::create([
+           'name' => 'Licenciatura en Informatica',
+           'educational_institution' => EducationalInstitutionEnum::UASD->value,
+            'educational_level' => EducationalLevelEnum::GRADE->value
+        ]);
+
+
     }
 }
